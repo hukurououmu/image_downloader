d26608f4423c76d406e5ce865992ede85d80d38b
Owl Parrot <50052625+hukurououmu@users.noreply.github.com>
2020-04-15 21:45:42 +0900

    Update picsdown.py

--- picsdown.py
+++ picsdown.py
@@ -1,76 +1,93 @@
 import os
-import sys
 import time
+from logging import basicConfig, getLogger, DEBUG
+
 import uuid
-from colorama import Fore
 from tqdm import tqdm
 import requests
 from requests.compat import urljoin
 from bs4 import BeautifulSoup
 
 
-try:
-    url = "{}".format(input("> Enter url: "))
-    while not url:
-        print(">" + Fore.RED + " Not entered" + Fore.RESET)
-        url = "{}".format(input("> Enter url: "))
-except KeyboardInterrupt:
-    print("\n>" + Fore.GREEN + " Program interrupted")
-    sys.exit()
+class Scraper:
+
+    def __init__(self):
+        self.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/75.0"
+        self.url = input("> Enter url: ")
+        self.img_format = input("> Enter format(jpg or png or gif): ")
 
 
-def getHTML():
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
-    }
-    res = requests.get(url, headers=headers)
-    time.sleep(1)
-    try:
-        if res.status_code == 200:
-            return res.content
-    except:
-        print(">" + Fore.RED + " Received a status code other than 200:{}".format(res.status_code))
-        sys.exit()
-
-
-def imgDownload():
-    try:
-        soup = BeautifulSoup(getHTML(), "lxml")
-    except:
-        soup = BeautifulSoup(getHTML(), "html5lib")
-
-    for link in tqdm(soup.find_all("img"), desc="> Downloading: "):
-        img_src = link.get("src")
-        if img_src.endswith(".jpg"):
-            yield urljoin(url, img_src)
-        elif img_src.endswith(".png"):
-            yield urljoin(url, img_src)
-        time.sleep(1)
-
-    print(">" + Fore.LIGHTGREEN_EX + " Download Complete")
-
-
-def save():
-    print("> Interrupt the program with Ctrl + C")
-    for img_file in imgDownload():
-        req = requests.get(img_file)
-        img_file = str(uuid.uuid4()) + ".jpg"
+    def get_html(self, url, headers=None):
+        logger = getLogger("get_html()")
         try:
-            if not os.path.exists("./img/"):
-                os.mkdir("img")
-            with open("./img/" + img_file, "wb") as f:
-                f.write(req.content)
-        except:
-            print(">" + Fore.RED + " Failed to save")
-            sys.exit()
+            time.sleep(3)
+            resp = requests.get(url, headers=headers)
+            try:
+                soup = BeautifulSoup(resp.text, "lxml")
+            except:
+                soup = BeautifulSoup(resp.text, "html5lib")
+            if resp.status_code == 200:
+                return soup
+            else:
+                raise Exception("Status code error")
+        except Exception as e:
+            logger.warning(e)
+            return None
+
+
+    def img_download(self):
+        logger = getLogger("img_download()")
+        try:
+            headers = {"User-Agent": self.user_agent}
+            soup = self.get_html(self.url, headers)
+            if soup != None:
+                for link in tqdm(soup.find_all("img"), desc="> Downloading..."):
+                    time.sleep(3)
+                    img_src = link.get("src")
+
+                    if img_src.endswith(".jpg"):
+                        yield urljoin(self.url, img_src)
+
+                    elif img_src.endswith(".png"):
+                        yield urljoin(self.url, img_src)
+                    
+                    elif img_src.endswith(".gif"):
+                        yield urljoin(self.url, img_src)
+                print("> Complete")
+            else:
+                raise Exception("No data")
+        except Exception as e:
+            logger.warning(e)
+            return None
+
+
+    def save_data(self):
+        logger = getLogger("save_data()")
+        try:
+            for img_file in self.img_download():
+                resp = requests.get(img_file)
+                img_file = str(uuid.uuid4()) + "." + self.img_format
+                try:
+                    if not os.path.exists("./img/"):
+                        os.mkdir("img")
+                    with open("./img/" + img_file, "wb") as f:
+                        f.write(resp.content)
+                except Exception as e:
+                    raise e
+        except Exception as e:
+            logger.warning(e)
+            return None
+
+
 
 
 def main():
     try:
-        save()
-    except KeyboardInterrupt:
-        print("\n>" + Fore.GREEN + " Program interrupted ")
-        sys.exit()
+        scraper = Scraper()
+        scraper.save_data()
+    except Exception as e:
+        print("> エラーになりました")
+        raise e
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
